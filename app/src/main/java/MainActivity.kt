9780a40c8d7ca3a4cc13e5814cc842b097a7c02c
TomokiHamada <t.hamada@fenrir.co.jp>
2025-12-14 15:48:41 +0900

    ✨ feat:  ボトムナビゲーションを用意

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -2,9 +2,90 @@ package io.novumd.gitseek
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import io.novumd.gitseek.io.novumd.gitseek.ui.bookmark.BottomTab
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            val tabs = remember {
+                listOf(
+                    BottomTab.Search(), // startDestination
+                    BottomTab.Bookmark()
+                )
+            }
+            val (selectedTab, onSelectedTabChanged) = remember { mutableStateOf(tabs.first()) }
+
+            LaunchedEffect(navController) {
+                navController.currentBackStackEntryFlow.collectLatest { entry ->
+                    val search = runCatching { entry.toRoute<BottomTab.Search>() }.getOrNull()
+                    search?.let { onSelectedTabChanged(it) }
+
+                    val bookmark = runCatching { entry.toRoute<BottomTab.Bookmark>() }.getOrNull()
+                    bookmark?.let { onSelectedTabChanged(it) }
+                }
+            }
+
+            Scaffold(
+                bottomBar = {
+                    NavigationBar {
+                        tabs.forEach { tab ->
+                            NavigationBarItem(
+                                selected = selectedTab == tab,
+                                onClick = {
+                                    onSelectedTabChanged(tab)
+                                    navController.navigate(tab) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                },
+                                icon = {
+                                    Icon(
+                                        imageVector = tab.icon,
+                                        contentDescription = stringResource(tab.labelRes)
+                                    )
+                                },
+                                label = { Text(stringResource(tab.labelRes)) }
+                            )
+                        }
+                    }
+                }
+            ) { padding ->
+                NavHost(
+                    navController = navController,
+                    startDestination = tabs.first(),
+                    modifier = Modifier.padding(padding)
+                ) {
+                    composable<BottomTab.Search> {
+                        Text("Search Screen")
+                    }
+                    composable<BottomTab.Bookmark> {
+                        Text("Bookmark Screen")
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/novumd/gitseek/ui/bookmark/BottomTab.kt
@@ -0,0 +1,34 @@
+package io.novumd.gitseek.io.novumd.gitseek.ui.bookmark
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+import io.novumd.gitseek.R
+import kotlinx.serialization.Serializable
+
+/**
+ * Bottom Navigation Barのタブ
+ */
+sealed interface BottomTab {
+    val labelRes: Int
+
+    val icon: ImageVector
+
+    @Serializable
+    data class Search(
+        @get:StringRes override val labelRes: Int = R.string.tab_search,
+    ) : BottomTab {
+        override val icon: ImageVector
+            get() = Icons.Filled.Search
+    }
+
+    @Serializable
+    data class Bookmark(
+        @get:StringRes override val labelRes: Int = R.string.tab_bookmark,
+    ) : BottomTab {
+        override val icon: ImageVector
+            get() = Icons.Filled.Favorite
+    }
+}
