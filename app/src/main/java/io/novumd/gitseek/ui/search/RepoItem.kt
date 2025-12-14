c395f1c1c5fd833a9be5c8b55b101cb711565392
TomokiHamada <t.hamada@fenrir.co.jp>
2025-12-14 18:44:34 +0900

    ✨ feat:  アバター画像を円で切り抜く

--- app/src/main/java/io/novumd/gitseek/ui/search/RepoItem.kt
+++ app/src/main/java/io/novumd/gitseek/ui/search/RepoItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Favorite
@@ -20,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -55,10 +57,10 @@ fun RepoItem(
             AsyncImage(
                 model = repo.owner.avatarUrl,
                 contentDescription = repo.owner.ownerName,
-                modifier = Modifier.size(40.dp),
+                modifier = Modifier.size(40.dp).clip(CircleShape),
                 placeholder = rememberVectorPainter(image = Icons.Filled.Sync),
                 error = rememberVectorPainter(image = Icons.Filled.Error),
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.Crop,
             )
 
             Spacer(Modifier.width(12.dp))
