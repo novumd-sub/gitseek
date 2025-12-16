57a119b5294931ec9f7ec3ed4f8a20d9d91babe0
TomokiHamada <t.hamada@fenrir.co.jp>
2025-12-16 23:16:55 +0900

    ✨ feat:  ユースケースのDIを整理

--- app/src/main/java/io/novumd/gitseek/di/UsecaseModule.kt
+++ app/src/main/java/io/novumd/gitseek/di/UsecaseModule.kt
@@ -4,8 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.novumd.gitseek.domain.usecase.ObserveBookmarksUseCase
+import io.novumd.gitseek.domain.usecase.ObserveBookmarksUseCaseImpl
 import io.novumd.gitseek.domain.usecase.SearchReposUseCase
 import io.novumd.gitseek.domain.usecase.SearchReposUsecaseImpl
+import io.novumd.gitseek.domain.usecase.ToggleBookmarkUseCase
+import io.novumd.gitseek.domain.usecase.ToggleBookmarkUseCaseImpl
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +18,12 @@ abstract class UsecaseModule {
     @Binds
     @Singleton
     abstract fun bindSearchReposUseCase(impl: SearchReposUsecaseImpl): SearchReposUseCase
+
+    @Binds
+    @Singleton
+    abstract fun bindObserveBookmarksUseCase(impl: ObserveBookmarksUseCaseImpl): ObserveBookmarksUseCase
+
+    @Binds
+    @Singleton
+    abstract fun bindToggleBookmarkUseCase(impl: ToggleBookmarkUseCaseImpl): ToggleBookmarkUseCase
 }
